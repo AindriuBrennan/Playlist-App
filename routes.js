3e759d4f2d21ea258cc3a297f31ffe91a3108cea
Aindriu <20079247@mail.wit.ie>
2019-11-04 17:46:13 +0000

    added accounts routes

--- routes.js
+++ routes.js
@@ -5,8 +5,9 @@ const router = express.Router();
 
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
-const playlist = require('./controllers/playlist.js');
-
+const playlist = require("./controllers/playlist.js");
+//route for accounts
+const accounts = require("./controllers/accounts.js");
 
 router.get("/", dashboard.index);
 //dashboard route
@@ -14,17 +15,23 @@ router.get("/dashboard", dashboard.index);
 //about route
 router.get("/about", about.index);
 //playlist id route
-router.get('/playlist/:id',playlist.index);
+router.get("/playlist/:id", playlist.index);
 //delete song button route
-router.get('/playlist/:id/deletesong/:songid',playlist.deleteSong);
+router.get("/playlist/:id/deletesong/:songid", playlist.deleteSong);
 //delete a playlist
-router.get('/dashboard/deleteplaylist/:id',dashboard.deletePlaylist);
-
-
+router.get("/dashboard/deleteplaylist/:id", dashboard.deletePlaylist);
 
 //add a song to playlist
-router.post('/playlist/:id/addsong', playlist.addSong);
-router.post('/dashboard/addplaylist', dashboard.addPlaylist);
+router.post("/playlist/:id/addsong", playlist.addSong);
+router.post("/dashboard/addplaylist", dashboard.addPlaylist);
 
+
+// routes for accounts
+router.get("/", accounts.index);
+router.get("/login", accounts.login);
+router.get("/signup", accounts.signup);
+router.get("/logout", accounts.logout);
+router.post("/register", accounts.register);
+router.post("/authenticate", accounts.authenticate);
 
 module.exports = router;
